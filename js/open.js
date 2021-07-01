4e4023d6b5cbd0281bc40bd3a182c5ce899ea37b
mplutarc <anda.ere@yandex.ru>
2021-07-02 00:17:08 +0300

    мобильная адаптация

--- js/open.js
+++ js/open.js
@@ -1,9 +1,27 @@
-$('button').click(function (){
-	$('.images').addClass('opened')
-	$(this).css('display', 'none')
-	$('.item1 p').html('Автопогрузчики')
-	$('.item3 p').html('Самоходныe подъёмники')
-	$('.item5 p').html('Электропогрузчики')
-	$('.item3 img').attr('src','img/img3-2.png')
-	$('.item5 img').attr('src','img/img5-2.png')
-})
+
+function changeItems(){
+	if ($(window).width() <= '768'){
+		$('.item3 p').html('Аккумуляторы и зарядные  устройства')
+		$('.item5 p').html('Техника б/у')
+		$('.item6 p').html('Колёса <span>индустриальные</span>')
+		$('.item7 p').html('Электропогрузчики')
+		$('.item8 p').html('Самоходные подъёмники')
+		$('.item9 p').html('Навесное оборудование')
+		$('.item10 p').html('Смазочные материалы')
+		$('.item11 p').html('Доп. оборудование')
+		$('button').click(function (){
+			$('.images').addClass('opened')
+			$(this).css('display', 'none')
+		})
+	} else {
+		$('button').click(function (){
+			$('.images').addClass('opened')
+			$(this).css('display', 'none')
+			$('.item1 p').html('Автопогрузчики')
+			$('.item3 p').html('Самоходныe подъёмники')
+			$('.item5 p').html('Электропогрузчики')
+		})
+	}
+}
+
+$(window).on('load resize', changeItems);
